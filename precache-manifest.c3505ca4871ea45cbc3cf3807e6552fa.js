0e2c5e17f144ff14d4b301f6d5717f6d94962e16
Tobias <tobias@appysphere.com>
2019-12-03 09:15:46 +0700

    Updates

---
+++ precache-manifest.c3505ca4871ea45cbc3cf3807e6552fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d7ed61cc6dc94cf41017db38a06f00",
+    "url": "/pomodoR/index.html"
+  },
+  {
+    "revision": "4b1c0728e03ad565ae2c",
+    "url": "/pomodoR/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "e5eb7eb1655f551be698",
+    "url": "/pomodoR/static/js/2.23c5dcc5.chunk.js"
+  },
+  {
+    "revision": "4b1c0728e03ad565ae2c",
+    "url": "/pomodoR/static/js/main.352fe5f6.chunk.js"
+  },
+  {
+    "revision": "27e2f489a14423b7df01",
+    "url": "/pomodoR/static/js/runtime-main.eb6ff061.js"
+  },
+  {
+    "revision": "89820ccd32a3279e833271b28092a236",
+    "url": "/pomodoR/static/media/sound.89820ccd.wav"
+  }
+]);
